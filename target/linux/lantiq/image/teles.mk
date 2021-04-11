f1985c17355805d8fea46450a66c3df44d736e65
Jens Bretschneider <mail@jens-bretschneider.de>
2021-04-11 17:09:29 +0000

    added IMAGE_SIZE

--- target/linux/lantiq/image/teles.mk
+++ target/linux/lantiq/image/teles.mk
@@ -2,6 +2,7 @@ define Device/teles_voipbox-dsl-bri
   $(Device/lantiqFullImage)
   DEVICE_VENDOR := Teles
   DEVICE_MODEL := VoIPBox DSL 2/4/8
+  IMAGE_SIZE := 6656k
 endef
 TARGET_DEVICES += teles_voipbox-dsl-bri
 
