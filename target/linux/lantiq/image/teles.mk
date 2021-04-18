ccc6bfb700980741f52c8bace84b9d4db7416a26
Jens Bretschneider <mail@jens-bretschneider.de>
2021-04-18 11:17:36 +0000

    Remove teles.mk (moved to vr9.mk)

--- target/linux/lantiq/image/teles.mk
+++
@@ -1,9 +0,0 @@
-define Device/teles_voipbox-dsl-bri
-  $(Device/lantiqFullImage)
-  DEVICE_VENDOR := Teles
-  DEVICE_MODEL := VoIPBox DSL 2/4/8
-  KERNEL_SIZE := 6656k
-  IMAGE_SIZE := 31232k
-endef
-TARGET_DEVICES += teles_voipbox-dsl-bri
-
