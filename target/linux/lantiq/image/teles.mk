759d48e4dce94b119c0c0d82c92c1b557331ef37
Jens Bretschneider <mail@jens-bretschneider.de>
2021-04-18 08:42:48 +0000

    KERNEL_SIZE and IMAGE_SIZE fixed

--- target/linux/lantiq/image/teles.mk
+++ target/linux/lantiq/image/teles.mk
@@ -2,7 +2,8 @@ define Device/teles_voipbox-dsl-bri
   $(Device/lantiqFullImage)
   DEVICE_VENDOR := Teles
   DEVICE_MODEL := VoIPBox DSL 2/4/8
-  IMAGE_SIZE := 6656k
+  KERNEL_SIZE := 6656k
+  IMAGE_SIZE := 31232k
 endef
 TARGET_DEVICES += teles_voipbox-dsl-bri
 
